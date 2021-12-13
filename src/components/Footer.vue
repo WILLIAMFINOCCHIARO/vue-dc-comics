f3bbfba9b58a88a3870c7b0e05d18cd722dc4ef0
William Finocchiaro <89465648+WILLIAMFINOCCHIARO@users.noreply.github.com>
2021-12-13 20:41:06 +0100

    almost

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,50 @@
 <template>
   <footer>
     <section class="one">
-      <div class="dc">
-      </div>
+      <div class="container">
+        <div class="link">
+          <ul>
+            <h2> dc comics </h2>
+              <li><a href="#">aaa</a></li>
+              <li><a href="#">aaa</a></li>
+              <li><a href="#">aaa</a></li>
+              <li><a href="#">aaa</a></li>
+              <li><a href="#">aaa</a></li>
+              <li><a href="#">aa</a></li>
+              <li><a href="#">aaa</a></li>
+          </ul>
 
+          <ul>
+            <h2>dc</h2>
+              <li><a href="#">aaa</a></li>
+              <li><a href="#">aaa</a></li>
+              <li><a href="#">aaa</a></li>
+              <li><a href="#">aaa</a></li>
+              <li><a href="#">aaa</a></li>
+              <li><a href="#">aa</a></li>
+              <li><a href="#">aaa</a></li>
+          </ul>
+          <ul>
+            <h2> sites</h2>
+              <li><a href="#">aaa</a></li>
+              <li><a href="#">aaa</a></li>
+              <li><a href="#">aaa</a></li>
+              <li><a href="#">aaa</a></li>
+              <li><a href="#">aaa</a></li>
+              <li><a href="#">aa</a></li>
+              <li><a href="#">aaa</a></li>
+          </ul>
+          <ul>
+            <h2> shop </h2>
+              <li><a href="#">aaa</a></li>
+              <li><a href="#">aaa</a></li>
+          </ul>
+          
+        </div>
+        <div class="logodc">
+        </div>
+
+      </div>
     </section>
   </footer>
 </template>
@@ -20,19 +61,44 @@ name:"Footer"
   background-image: url("../assets/img/footer-bg.jpg");
   background-position: center;
   background-size: cover;
-    .dc {
+    .logodc {
       height:350px;
       background-image: url("../assets/img/dc-logo-bg.png");
       background-repeat: no-repeat;
       background-position:90%,right;
     }
-    ul {
+  .link {
+    width:40%;
+      ul {
       float:left;
+      color:white;
+      text-align: center;
+      margin:0 15px;
+      padding-top:30px;
+      li {
+        list-style: none;
+        h2 {
+          font-size:20px;
+          text-transform: uppercase;
+        }
+        a {
+          font-size: 15px;
+          text-decoration: none;
+          color:grey;
+          margin:20px;
+          
+        }
+        
+      }
+        
     }
+  }
     img {
       height:500px;
       float:right;
       padding-right: 90px;
     }
+
+    
 }
 </style>
